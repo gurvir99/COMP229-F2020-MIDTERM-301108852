00e56d06b27b7ae45760a646be29d28cce969a2c
gurvir99 <singh.gurvir37@gmail.com>
2020-10-26 21:46:47 -0400

    Added Edit and Delete Button. Added code to update document.

--- server/routes/books.js
+++ server/routes/books.js
@@ -29,6 +29,7 @@ router.get('/add', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+    res.render('books/details', {title: 'Add Book'});
 
 });
 
@@ -38,23 +39,81 @@ router.post('/add', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+    let newBook = Book({
+      "Title": req.body.Title,
+      "Description": req.body.Description,
+      "Price": req.body.Price,
+      "Author": req.body.Author,
+      "Genre": req.body.Genre
+   });
+
+   Book.create(newBook,(err,Book)=>{
+       if(err)
+       {
+           console.log(err);
+           res.end(err);
+       }
+       else
+       {
+           //refresh the book list
+           res.redirect('/books')
+       }
+   })
+
 
 });
 
 // GET the Book Details page in order to edit an existing Book
-router.get('/:id', (req, res, next) => {
+router.get('/details/:id', (req, res, next) => {
 
     /*****************
      * ADD CODE HERE *
      *****************/
+    let id = req.params.id;
+
+  book.findById(id,(err, bookToEdit) => {
+     if(err)
+     {
+         console.log(err);
+         res.end(err);
+     }
+     else
+     {
+         //show the edit view
+         res.render('books/details', {title:'Edit Book', book:bookToEdit})
+     }
+  });
 });
 
 // POST - process the information passed from the details form and update the document
-router.post('/:id', (req, res, next) => {
+router.post('/details/:id', (req, res, next) => {
 
     /*****************
      * ADD CODE HERE *
      *****************/
+    let id = req.params.id
+
+    let updatedBook = book({
+        "_id":id,
+        "Title": req.body.title,
+        "Description": req.body.description,
+        "Price": req.body.price,
+        "Author": req.body.author,
+        "Genre": req.body.genre
+    });
+
+    book.updateOne({_id: id}, updatedBook, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the book list
+            res.redirect('/books');
+        }
+    });
 
 });
 
