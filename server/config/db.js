fe1bb8bb5f8df83d742267134afc298ca2e51532
gurvir99 <singh.gurvir37@gmail.com>
2020-10-29 13:02:42 -0400

    Updated URI for MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/book_store"
+  // "URI": "mongodb://localhost/book_store"
+  "URI": "mongodb+srv://gurvir:UeVCZc72kCJa9Hty@mongodbserver.1m6hr.mongodb.net/book_store?retryWrites=true&w=majority"
 };
